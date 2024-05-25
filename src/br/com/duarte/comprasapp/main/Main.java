cdb48289f2c4052eade5cae75c20f56ff7b0345d
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-05-25 17:11:50 -0300

    fix: resolucao do erro de entrada via teclado para nomes com espaco

--- src/br/com/duarte/comprasapp/main/Main.java
+++ src/br/com/duarte/comprasapp/main/Main.java
@@ -3,8 +3,12 @@ package br.com.duarte.comprasapp.main;
 import br.com.duarte.comprasapp.model.LimiteCartaoDeCredito;
 import br.com.duarte.comprasapp.model.SaldoCartaoDebito;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
 //        LimiteCartaoDeCredito cartaoDeCredito = new LimiteCartaoDeCredito();
 //
 //        cartaoDeCredito.setNomeDoTitular("Rodrigo Duarte");
@@ -17,11 +21,22 @@ public class Main {
 
         SaldoCartaoDebito cartaoDebito = new SaldoCartaoDebito();
 
-        cartaoDebito.setNomeDoTitular("Samira Duarte da Silva");
-        cartaoDebito.setNumeroDoCartao("4321 4321 4321 4321");
-        cartaoDebito.setBandeira("Master Card");
-        cartaoDebito.setBancoEmissor("Banco do Brasil");
-        cartaoDebito.setSaldoEmConta(20000.00);
+        System.out.print("Digite o nome do titular do cartão: ");
+        cartaoDebito.setNomeDoTitular(scanner.nextLine());
+
+        System.out.print("Digite o número do cartão: ");
+        cartaoDebito.setNumeroDoCartao(scanner.nextLine());
+
+        System.out.print("Digite a bandeira do cartão: ");
+        cartaoDebito.setBandeira(scanner.nextLine());
+
+        System.out.print("Digite o nome do banco ao qual o cartão está vinculado: ");
+        cartaoDebito.setBancoEmissor(scanner.nextLine());
+
+        System.out.print("Informe o saldo atual em conta: ");
+        cartaoDebito.setSaldoEmConta(scanner.nextDouble());
+
+        scanner.close();
 
         System.out.println(cartaoDebito.getMensagem());
     }
